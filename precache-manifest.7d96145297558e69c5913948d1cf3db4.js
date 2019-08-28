d3a389c0112ef6a5b733578f2accd1cba25d1d73
Sarfulah Stewart <fu.stew@yahoo.com>
2019-08-28 13:06:40 -0400

    Updates

---
+++ precache-manifest.7d96145297558e69c5913948d1cf3db4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273b09da93285ba851400f8a59d0d3e6",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "01693b28093b1e07eaf8",
+    "url": "/Clicky-Game/static/css/main.da75fde0.chunk.css"
+  },
+  {
+    "revision": "c781d8d14aa25daf4a98",
+    "url": "/Clicky-Game/static/js/2.0fb7f9dc.chunk.js"
+  },
+  {
+    "revision": "01693b28093b1e07eaf8",
+    "url": "/Clicky-Game/static/js/main.9335d3d4.chunk.js"
+  },
+  {
+    "revision": "75fa83e2a97bc1c69d72",
+    "url": "/Clicky-Game/static/js/runtime~main.4267543b.js"
+  }
+]);
